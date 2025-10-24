080c22feaa676a32adadc4b307b6c0a50b47e8db
am <a.michael1@uq.edu.au>
2025-10-24 21:52:06 +1000

    cli

--- bin/main.ml
+++ bin/main.ml
@@ -1,4 +1,17 @@
+open Cmdliner
 
+let check fname = Ocaml_of_basil.Loadir.ast_of_fname fname |> ignore
 
-let () =
-  print_endline "Hello, World!";
+let fname =
+  let doc = "Input file name (filename.il)" in
+  Arg.(required & pos 0 (some string) None & info [] ~docv:"FNAME" ~doc)
+
+let check_f = Term.(const check $ fname)
+
+let cmd =
+  let doc = "obasil" in
+  let info = Cmd.info "obasil" ~version:"alpha" ~doc in
+  Cmd.v info check_f
+
+let main () = exit (Cmd.eval cmd)
+let () = main ()
